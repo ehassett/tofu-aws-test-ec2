c4c9ea4378dc6a826ac4e39bc41dda5b3ba8a9dc
Ethan Hassett <ethan@hassett.io>
2024-08-28 17:46:03 +0000

    fix: address checkov errors

--- main.tf
+++ main.tf
@@ -42,14 +42,24 @@ resource "aws_security_group_rule" "allow_workstation_ingress" {
 }
 
 resource "aws_instance" "this" {
+  #checkov:skip=CKV_AWS_79:Ignoring metadata options
+  #checkov:skip=CKV_AWS_88:Instance needs a public IP
+  #checkov:skip=CKV2_AWS_41:Instance does not need an IAM instance profile
+
   ami                         = var.ami_id == null ? data.aws_ami.this[0].id : var.ami_id
   associate_public_ip_address = true
   availability_zone           = format("%sa", var.region)
+  ebs_optimized               = true
   get_password_data           = var.get_password_data
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.this.key_name
+  monitoring                  = true
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = [aws_security_group.this.id]
+
+  root_block_device {
+    encrypted = true
+  }
 
   tags = {
     Name = var.name
